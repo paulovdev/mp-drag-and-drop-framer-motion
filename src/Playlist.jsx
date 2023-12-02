57504d789e301e1083e11b72cda35800a45d79bb
pxulin <fudevil123@gmail.com>
2023-12-02 13:05:48 -0300

    new

--- src/Playlist.jsx
+++ src/Playlist.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import { Reorder } from "framer-motion";
 import { RiDraggable } from "react-icons/ri";
 
-<<<<<<< HEAD
-
-=======
->>>>>>> cb617efe33a58fbcaf985846ec9797957cb6551d
 const Playlist = ({ item }) => {
   return (
     <Reorder.Item value={item} id={item.song}>
@@ -18,18 +14,10 @@ const Playlist = ({ item }) => {
             <h6>{item.artist}</h6>
           </div>
         </div>
-<<<<<<< HEAD
-        <span>{item.duration}</span>
-=======
         <span>{item.time}</span>
->>>>>>> cb617efe33a58fbcaf985846ec9797957cb6551d
       </div>
     </Reorder.Item>
   );
 };
 
-<<<<<<< HEAD
 export default Playlist;
-=======
-export default Playlist;
->>>>>>> cb617efe33a58fbcaf985846ec9797957cb6551d
